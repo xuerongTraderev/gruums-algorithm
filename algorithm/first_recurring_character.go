378905fa0177ca8e2432c540b11ef427a8bce441
xuerongTraderev <xuerong.wu@traderev.com>
2019-08-12 18:07:49 -0400

    initial project

---
+++ algorithm/first_recurring_character.go
@@ -0,0 +1,8 @@
+package algorithm
+
+import (
+)
+
+func FirstRecurringCharacter() String {
+	return "afdsasdfasfd"
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"algorithm"
+)
+
+func main() {
+	fmt.Println("Hello world")
+}
